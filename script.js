01384527db404d60dd074df2360bbd640cb1910f
Luan <luam10@hotmail.com.br>
2024-01-27 23:04:55 -0300

    sestilização

---
+++ script.js
@@ -0,0 +1,12 @@
+const input1 = document.getElementById('input1').value
+const input2 = document.getElementById('input2').value
+const form = document.getElementById('formValidar')
+
+form.addEventListener('click', function(){
+    
+})
+
+
+function validacaoinput(){
+
+}
