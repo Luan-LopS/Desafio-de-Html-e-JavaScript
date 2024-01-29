2f7367de85e6d63f413e06197e6bfcf317b67f9e
Luan <luam10@hotmail.com.br>
2024-01-28 23:44:51 -0300

    quase la a validação

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-const input1 = parseInt(document.getElementById('input1'))
-const input2 = parseInt(document.getElementById('input2'))
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
 const form = document.getElementById('formValidar')
+
+
 
 form.addEventListener('submit', function(e){
     e.preventDefault()
+
 })
 
-
-input2.addEventListener('keyup', function(e){
+input2.addEventListener('keyup',function(e){
     console.log(e.target.value)
-    let inputMenor = input1.value
-    let inputMaior = input2.value
 
-    let erro = document.querySelector('.numeroMaior')
+    let valorInput1 = parseInt(input1.value)
+    let valorInput2 = parseInt(e.target.value)
+    let p = document.querySelectorAll('p')[1]
+    p.innerHTML = `Número B tem que ser maior que o A`
 
-    if(inputMaior > inputMenor){
-        console.log('certo')
-        erro.classList.add('numeroMaior')
-    }else{
-        console.log('errados')
+    if(valorInput1 < valorInput2){
+        console.log('2 é maior')
+        p.classList.remove('numeroMaior')
+        p.innerHTML = ''
+
+    } else{
+        console.log('1 é maior')
+        p.classList.add('numeroMaior')
 
     }
 
