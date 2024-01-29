e193daf1ca4e26a75209b45bf01649c23ed7845c
Luan Lopes <81537039+Luan-LopS@users.noreply.github.com>
2024-01-29 17:39:31 -0300

    valid falta verificar se são numeros

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const input1 = document.getElementById('input1')
 const input2 = document.getElementById('input2')
 const form = document.getElementById('formValidar')
+const button = document.querySelector('button')
 
-
-
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-
-})
 
 input2.addEventListener('keyup',function(e){
     console.log(e.target.value)
@@ -15,16 +10,19 @@ input2.addEventListener('keyup',function(e){
     let valorInput1 = parseInt(input1.value)
     let valorInput2 = parseInt(e.target.value)
     let p = document.querySelectorAll('p')[1]
-    p.innerHTML = `Número B tem que ser maior que o A`
-
+    let msgNegativa = `Número B tem que ser maior que o A`
+    let msgPositiva = `Pode Enviar `
 
     if(valorInput1 < valorInput2){
         console.log('2 é maior')
-        p.classList.remove('numeroMaior')
-        p.innerHTML = ''
-
+        p.classList.add('numeroMenor')
+        p.innerHTML = msgPositiva
+        button.disabled = false
+        form.style.display = 'none'
     } else{
         console.log('1 é maior')
         p.classList.add('numeroMaior')
+        p.innerHTML =  msgNegativa
+        button.disabled = true
     }
 })
